457bf865ee7282b40f5686abb3d0af1b0944b768
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-07-11 22:02:58 +0700

    Initial Commit

---
+++ SwiftUI2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI2.0
+//
+//  Created by Quốc Huy Nguyễn on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI2.0/SwiftUI2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2_0App.swift
+//  SwiftUI2.0
+//
+//  Created by Quốc Huy Nguyễn on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
